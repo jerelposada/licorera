e43ec7600aafd38f46e1c5627b99e0a85fd42e8a
jerel posada <jerelposada@gmail.com>
2021-02-11 22:08:50 -0500

    proyecto con validaciones y campo carga de archivo, falta la logica de la carga de archivo

--- routes/backendRoutes.js
+++ routes/backendRoutes.js
@@ -2,14 +2,19 @@ const express = require ('express');
 const router = express.Router();
 
 const homeControllers = require('../lib/controllers/backend');
-
+const { body } = require('express-validator');
 
  module.exports = function() {
     router.get('/', homeControllers.adminHome);
 
     router.get('/nuevoProducto',homeControllers.formularioProducto);
 
-    router.post('/nuevoProducto',homeControllers.nuevoProducto);
+    router.post('/nuevoProducto',
+    body('nombre').not().isEmpty().escape().trim(),
+    body('descriptionShort').not().isEmpty().trim().escape(),
+    body('descriptionLong').not().isEmpty().trim().escape(),
+    body('Cantidad').not().isEmpty().trim().escape(),
+    homeControllers.nuevoProducto);
 
     return router;
  }
